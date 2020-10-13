f0790a1556eb7b95e8d6961089c785b35ae98d24
veljkostevanic1996 <stevanicveljko1996@gmail.com>
2020-10-13 05:34:37 -0700

    create spock test

--- src/main/java/spock/SpockClass.groovy
+++ src/main/java/spock/SpockClass.groovy
@@ -5,10 +5,11 @@ import spock.lang.Specification
 class SpockClass extends Specification{
 
     int sum
-
+    int petica
     def setup(){
 
         sum = 2
+        petica = 5
         println("suma je inicijalizovana")
     }
 
@@ -18,5 +19,19 @@ class SpockClass extends Specification{
         println("provera")
         1 + 1 == sum
 
+    }
+
+    def 'block in spock'(){
+
+        given: 'set value'
+        int a = 2
+        int b = 3
+
+        when: ''
+        int c = a + b
+
+        then:
+        c == petica
+        println("$c je jednak $petica")
     }
 }
